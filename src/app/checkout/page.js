0e35c4e395a72a4e866fd57c68b02fd69a197ad3
fortune <omorodion.fortune@gmail.com>
2024-07-09 21:58:45 +0100

    fixes

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,17 +1,58 @@
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Page = () => {
+  const [items, setItems] = useState([
+    { id: 1, name: "Casio CA-500WE-1AEF", price: 160002, quantity: 1 },
+    { id: 2, name: "Mondaine Classic 40mm", price: 160002, quantity: 1 },
+  ]);
+
+  // // Function to handle incrementing quantity
+  // const handleIncrement = (index) => {
+  //   const updatedItems = [...items];
+  //   updatedItems[index].quantity++;
+  //   setItems(updatedItems);
+  // };
+
+  // // Function to handle decrementing quantity
+  // const handleDecrement = (index) => {
+  //   const updatedItems = [...items];
+  //   updatedItems[index].quantity = Math.max(
+  //     updatedItems[index].quantity - 1,
+  //     1
+  //   );
+  //   setItems(updatedItems);
+  // };
+
+  // // Function to calculate total price
+  // const calculateTotal = () => {
+  //   let total = 0;
+  //   items.forEach((item) => {
+  //     total += item.price * item.quantity;
+  //   });
+  //   return total;
+  // };
+
+  
+  // const removeFromCart = (index) => {
+  //   const updatedItems = [...items];
+  //   updatedItems.splice(index, 1);
+  //   setItems(updatedItems);
+  // };
+
   return (
     <div className="w-full md:px-[5rem] py-0 px-0 md:py-[2rem] bg-[#f4f4f479]">
       <Link href="/">
-        <Image
-          src="/logo.svg"
-          alt="logo"
-          width={150}
-          height={50}
-          className="md:px-0 md:py-0 px-[2rem] py-[1rem]"
-        />
+        <a>
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={150}
+            height={50}
+            className="md:px-0 md:py-0 px-[2rem] py-[1rem]"
+          />
+        </a>
       </Link>
       <div className="md:flex grid gap-10 w-full">
         <div className="md:py-[5rem] md:px-0 w-full grid md:gap-10">
@@ -101,7 +142,6 @@ const Page = () => {
                   alt="polygon"
                   width={15}
                   height={15}
-                  
                 />
               </div>
             </div>
@@ -118,7 +158,12 @@ const Page = () => {
                 <span className="text-[#0F172A]">UPS</span>
               </div>
               <div>
-                <Image src="/polygon.svg" alt="polygon" width={15} height={15} />
+                <Image
+                  src="/polygon.svg"
+                  alt="polygon"
+                  width={15}
+                  height={15}
+                />
               </div>
             </div>
             <div className="px-10 pb-7 flex flex-col gap-5">
@@ -155,7 +200,12 @@ const Page = () => {
                   width={44}
                   height={28}
                 />
-                <Image src="/polygon.svg" alt="polygon" width={15} height={15} />
+                <Image
+                  src="/polygon.svg"
+                  alt="polygon"
+                  width={15}
+                  height={15}
+                />
               </div>
             </div>
             <div className="pb-[2rem] flex gap-5">
